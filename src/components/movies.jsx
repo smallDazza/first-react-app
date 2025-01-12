8edbe8d8c0bfce92ca8a188bf07ed6344c224492
smallDazza <smalldazza@gmail.com>
2025-01-12 14:08:03 +1100

    Complete: finished the movie list, my first react app git add .

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -7,32 +7,44 @@ class Movies extends Component {
         movies: getMovies()
     };
 
-    handleDelete = () => {
-
+    handleDelete = (movie) => {
+        const movies = this.state.movies.filter(m => m._id !== movie._id);
+        this.setState({movies: movies});
     };
 
     render() {
-        return <table className="table">
+        if (this.state.movies.length === 0)
+            return ( <p>There are no movies in the database.</p>)
+        // these table elements layout got from bootstrap/ tables website:
+        return (
+            <>
+            <p>Showing {this.state.movies.length} movies in the database.</p>
+            <table className="table">
             <thead>
                 <tr>
                     <th>Title</th>
                     <th>Genre</th>
                     <th>Stock</th>
                     <th>Rate</th>
+                    <th></th>
                 </tr>
             </thead>
             <tbody>
                 {this.state.movies.map(movie =>
-                <tr>
+                <tr key={movie._id}>
                     <td>{movie.title}</td>
                     <td>{movie.genre.name}</td>
                     <td>{movie.numberInStock}</td>
                     <td>{movie.dailyRentalRate}</td>
+                    <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger">Delete</button></td>
                 </tr>
                 )}
                 
             </tbody>
-        </table>
+            </table>
+            </>
+            
+        );
     };
 }
 
